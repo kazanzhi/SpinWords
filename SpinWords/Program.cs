05752453cef44796a821c7e5dc7396b152c2f1ad
kazanzhi <z.kazanzhi2017@gmail.com>
2022-01-27 17:22:50 +0200

    SpinWords

---
+++ SpinWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SpinWords
+{
+    class Program
+    {
+        public static string SpinWordsBetter(string incoming) =>
+            string.Join(" ",
+                incoming.Split(new char[] { ' ' })
+                .Select(s => s.Length >= 5 ? string.Join("", s.Reverse()) : s));
+
+        static void Main(string[] args)
+        {
+            var w = SpinWordsBetter("emocleW");
+            var m = SpinWordsBetter("olleH yadnoM");
+            var t = SpinWordsBetter("This is a test");
+            var y = SpinWordsBetter("You are tsomla to the last test");
+            var j = SpinWordsBetter("Just gniddik ereht is llits one more");
+            Console.WriteLine(w);
+            Console.WriteLine(m);
+            Console.WriteLine(t);
+            Console.WriteLine(y);
+            Console.WriteLine(j);
+        }
+    }
+}
+
